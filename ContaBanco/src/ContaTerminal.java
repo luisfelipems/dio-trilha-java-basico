50cd28f6aca705a90d40ad61f35fdfbe7ef2e20e
Felipe <luisfelipe.martinssilva@gmail.com>
2024-08-01 14:03:00 -0300

    Conclusão do desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    /*
+        Link do projeto
+        https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
+    */
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*  To Do
+        * Conhece e importar a classe Scanner
+        * Exibir as mensagens ao usuário 
+        Obter pela classe Scanner os valores digitados pelo usuário via terminal
+        */ 
+;
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o nome da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu usuário: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Olá " + nomeCliente + ", obrigador por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e o seu saldo R$ " + saldo + " já está disponível para saque.");
     }
 }
